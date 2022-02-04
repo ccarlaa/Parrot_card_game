4aec27e3d5211ffb23ef33d5643f2eac2f4fc494
ccarlaa <carlacarlaclementino@gmail.com>
2022-02-04 16:15:54 -0300

    Rotaciona as 2 castas escolhidas após a escolha da segunda

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let qtdcartas = 0;
 let par = 0;
 let div = [];
-let img = ["arquivosuteis/unicornparrot.gif", "arquivosuteis/tripletsparrot.gif", "arquivosuteis/metalparrot.gif", "arquivosuteis/revertitparrot.gif", "arquivosuteis/fiestaparrot.gif", "arquivosuteis/explodyparrot.gif",  "arquivosuteis/bobrossparrot.gif",]
+let img = ["unicornparrot", "tripletsparrot", "metalparrot", "revertitparrot", "fiestaparrot", "explodyparrot", "bobrossparrot"]
 let imagenscolhidas = []
+let cartaclicada
+let primeiracarta
+let segundacarta 
 
+// ---------PROMPT - QUANTIDADE DE CARTAS----------
 
 while(qtdcartas<4 || qtdcartas>15 || par!==0){
     qtdcartas=prompt("Quantas cartas...?");
     qtdcartas=parseInt(qtdcartas);
     par = qtdcartas%2;
 }
+
+// ---------EMBARALHAR CARTAS -----------------
 
 for(let i=0; i < qtdcartas/2; i++){
     imagenscolhidas.push(img[i]);
@@ -22,11 +28,13 @@ function comparador() {
 
 imagenscolhidas.sort(comparador)
 
+// ----------CRIAR DIVS DINÂMICAS ------------
+
 for(i = 0; i < qtdcartas; i++){          
     div[i] =(`
-    <div class="umacarta" onclick="virarCarta(this)">
+    <div class="umacarta">
         <div class="frente face rotacionar">
-            <img src="${imagenscolhidas[i]}">
+            <img src="arquivosuteis/${imagenscolhidas[i]}.gif">
         </div>
         <div class="verso face">
             <img src="arquivosuteis/frente1.png">
@@ -37,8 +45,23 @@ for(i = 0; i < qtdcartas; i++){
     addcartas.innerHTML+=div[i];
 }
 
+// -----------ROTACIONAR CARTAS AO CLICAR 
 
-function virarCarta(divselecionada){
-   divselecionada.classList.toggle("rotacionar")
+let todasascartas = document.querySelectorAll(".umacarta")
+
+function virarCarta(){
+    if(!primeiracarta){
+        primeiracarta = this;
+        return false;
+    }
+
+    segundacarta = this;
+
+    if(primeiracarta!=null && segundacarta!=null){
+        this.classList.toggle("rotacionar");
+        primeiracarta.classList.toggle("rotacionar")
+    }
 }
+
+todasascartas.forEach(carta => carta.addEventListener('click', virarCarta))
 
